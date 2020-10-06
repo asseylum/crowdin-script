faadfdd0a9ad1577069486b9b631aadb3252a455
Uladzislau Yakauleu <u.yalauleu@gmail.com>
2020-10-06 13:10:45 +0300

    init

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const admZip = require("adm-zip");
 const localesDir = `${process.cwd()}/${process.env.LOCALES_DIR}`;
 const PROJECT = process.env.CROWDIN_PROJECT;
 const KEY = process.env.CROWDIN_KEY;
-const builtUrl = `https://api.crowdin.com/api/project/${PROJECT}/export?key=${KEY}`;
-const downloadUrl = `https://api.crowdin.com/api/project/${PROJECT}/download/all.zip?key=${KEY}`;
+const url = "https://api.crowdin.com/api/project";
+const builtUrl = `${url}/${PROJECT}/export?key=${KEY}`;
+const downloadUrl = `${url}/${PROJECT}/download/all.zip?key=${KEY}`;
 
 async function main() {
   console.log("Creating latest build...");
